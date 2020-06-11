696536afc214372fda027fa990b4b217272ff007
worasitipon <worasitipon.t@hotmail.com>
2020-06-11 15:41:09 +0700

    add Greet component

---
+++ src/components/Greet.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<h4>Async component from Greet</h4>
+	</div>
+</template>
+
+<script lang="ts">
+// import Vue from 'vue';
+export default {
+	name: 'Greet',
+};
+</script>
+
+<style scoped>
+div {
+	padding: 0.5rem;
+	background-color: burlywood;
+	color: #333;
+}
+</style>
