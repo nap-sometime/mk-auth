682f7db2ddbe95ef4ebac6f46a994fa0d58c3563
worasitipon <worasitipon.t@hotmail.com>
2020-06-11 18:58:55 +0700

    add page login

---
+++ src/components/PageLogin.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+		<form>
+			<label for="username">ชื่อผู้ใช้</label>
+			<input v-model.trim="username" type="text" name="username" />
+			<label for="password">รหัสผ่าน</label>
+			<input v-model.trim="password" type="password" name="password" />
+			<button type="submit" @click.prevent="onLogin">ล็อคอิน</button>
+		</form>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { setAuthInfo } from '@/utils/auth';
+
+export default Vue.extend({
+	data: () => ({
+		username: '',
+		password: '',
+	}),
+	methods: {
+		onLogin() {
+			setAuthInfo({ username: this.username, password: this.password });
+			this.$router.replace('/home');
+		},
+	},
+});
+</script>
