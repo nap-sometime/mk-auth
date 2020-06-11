42376f0c4de3bc8a0ccdfca469cbfac3d6f2c8cd
worasitipon <worasitipon.t@hotmail.com>
2020-06-11 14:56:18 +0700

    add router view on root el

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,17 @@
 <template>
-	<div id="app">
-		<img alt="Vue logo" src="./assets/logo.png" />
-		<HelloWorld msg="Welcome to Your bar x" />
+	<div id="mk-auth">
+		<router-view></router-view>
 	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
 export default {
 	name: 'App',
-	components: {
-		HelloWorld,
-	},
 };
 </script>
 
 <style scoped>
-#app {
+#mk-auth {
 	font-family: Avenir, Helvetica, Arial, sans-serif;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
