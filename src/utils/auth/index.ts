1b23f1ceb3d0625294a78b1c929b3dcae4729791
wornut <worasitipon.t@hotmail.com>
2020-06-12 22:27:24 +0700

    update auth utils

--- src/utils/auth/index.ts
+++ src/utils/auth/index.ts
@@ -1,12 +1,27 @@
 import cookie from 'js-cookie'
 import { NavigationGuard } from 'vue-router'
 
+export const authCookieName = 'auth'
+
 export function isAuthenticated() {
 	return Boolean(cookie.get('auth'))
 }
 
-export function setAuthInfo(data: any) {
-	cookie.set('auth', data, { expires: 7 })
+export interface IAuthInfo {
+	username: string
+	login_id: string
+}
+
+export function getAuthInfo(params?: Partial<IAuthInfo>): IAuthInfo | null {
+	return JSON.parse(cookie.get(authCookieName) || 'null')
+}
+
+export function setAuthInfo(data: IAuthInfo) {
+	cookie.set(authCookieName, JSON.stringify(data), { expires: 7 })
+}
+
+export function removeAuthInfo() {
+	cookie.remove(authCookieName)
 }
 
 export const requireAuthentication:NavigationGuard  = (to, _from, next) => {
