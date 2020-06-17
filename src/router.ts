2dd690d8749b88fabcf909cb7753d586d6db9fb2
worasitipon <worasitipon.t@hotmail.com>
2020-06-17 18:55:56 +0700

    update auth router

--- src/router.ts
+++ src/router.ts
@@ -2,8 +2,9 @@ import Router, { RouteConfig } from 'vue-router'
 import { requireAuthentication } from '@/utils/auth';
 
 const routes: RouteConfig[] = [
-	{ path: '/', component: () => import('@/components/Logo.vue') },
-	{ path: '/auth/login', name: 'PageLogin', component: () => import('@/components/PageLogin.vue') }
+	{ path: '/', redirect: '/home' },
+	{ path: '/auth/login', name: 'PageLogin', component: () => import('@/components/PageLogin.vue') },
+	{ path: '/auth/logout', name: 'PageLogout', component: () => import('@/components/Logo.vue') }
 ]
 
 const router = new Router({
