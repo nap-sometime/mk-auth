607471365942315bd1f243b574cf4b0aab99c330
worasitipon <worasitipon.t@hotmail.com>
2020-06-24 16:07:02 +0700

    improve vue config

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,29 @@ const path = require('path');
 const { readFileSync } = require('fs');
 const { name: npm_package_name } = require('./package.json');
 
+// pass thought client env via VUE_APP_*
 process.env.VUE_APP_NAME = npm_package_name;
 
+// helper
+const isProd = ['production', 'runtest'].some((v) => process.env.NODE_ENV === v);
 const loadFile = (path = '') => readFileSync(path);
+
+// config webpack dev server
+const devServer = isProd
+	? undefined
+	: {
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+			},
+			disableHostCheck: true,
+			sockPort: 8001,
+			sockHost: 'localhost',
+			port: 8001,
+			https: {
+				cert: loadFile(path.join(__dirname, '../cert/cert.pem')),
+				key: loadFile(path.join(__dirname, '../cert/key.pem')),
+			},
+	  };
 
 module.exports = {
 	lintOnSave: false,
@@ -22,18 +42,6 @@ module.exports = {
 				'~': path.resolve(__dirname, 'src'),
 			},
 		},
-		devServer: {
-			headers: {
-				'Access-Control-Allow-Origin': '*',
-			},
-			disableHostCheck: true,
-			sockPort: 8001,
-			sockHost: 'localhost',
-			port: 8001,
-			https: {
-				cert: loadFile(path.join(__dirname, '../cert/cert.pem')),
-				key: loadFile(path.join(__dirname, '../cert/key.pem')),
-			},
-		},
+		devServer,
 	},
 };
