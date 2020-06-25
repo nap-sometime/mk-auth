1d0e32662d1cc8bf9fdb22cb6fdb97a5a89ec4c4
worasitipon <worasitipon.t@hotmail.com>
2020-06-25 16:48:26 +0700

    update login page component

--- src/components/PageLogin.vue
+++ src/components/PageLogin.vue
@@ -1,37 +1,15 @@
 <template>
-	<div class="main">
-		<form>
-			<label for="username">ชื่อผู้ใช้</label>
-			<input v-model.trim="username" type="text" name="username" />
-			<label for="password">รหัสผ่าน</label>
-			<input v-model.trim="password" type="password" name="password" />
-			<button type="submit" @click.prevent="onLogin">ล็อคอิน</button>
-		</form>
+	<div class="relative h-screen w-screen bg-gray-100 flex items-center justify-center">
+		<FormLogin />
 	</div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import { setAuthInfo } from '@/utils/auth';
 
 export default Vue.extend({
-	data: () => ({
-		username: '',
-		password: '',
-	}),
-	methods: {
-		onLogin(): void {
-			setAuthInfo({ username: this.username, login_id: String(Date.now()) });
-			this.$router.replace('/home');
-		},
+	components: {
+		FormLogin: () => import('@/components/FormLogin.vue'),
 	},
 });
 </script>
-
-<style scoped>
-.main {
-	height: 100%;
-	width: 100%;
-	background: #888;
-}
-</style>
